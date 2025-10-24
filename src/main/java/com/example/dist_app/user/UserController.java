521d318d0b347f450cb48f061e56e6248bda6d9b
Dominik Wies <domi.wies@googlemail.com>
2025-10-24 06:08:11 +0200

    1-Create user

--- src/main/java/com/example/dist_app/user/UserController.java
+++ src/main/java/com/example/dist_app/user/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final IUserService userService;
@@ -24,4 +24,34 @@ public class UserController {
         return this.userService.getUserById(id);
     }
 
+    @PostMapping("/create")
+    public User create(
+        @RequestParam String firstname,
+        @RequestParam String lastname,
+        @RequestParam String email,
+        @RequestParam(required = false) String zipcode,
+        @RequestParam(required = false) String city,
+        @RequestParam(required = false) String state,
+        @RequestParam(required = false) String country,
+        @RequestParam Gender gender
+    ) {
+        User user = new User(
+            6L,
+                firstname,
+                lastname,
+                email,
+                new Address(
+                    6L,
+                    zipcode,
+                    city,
+                    state,
+                    country
+                ),
+                gender
+        );
+
+        UserService.addUser(user);
+
+        return user;
+    }
 }
--- src/main/java/com/example/dist_app/user/UserService.java
+++ src/main/java/com/example/dist_app/user/UserService.java
@@ -2,28 +2,37 @@ package com.example.dist_app.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserService implements IUserService {
 
+    private static final List<User> users = new ArrayList<>(
+        List.of(
+            new User(1L, "Sheev", "Palpatine", "sheev.palpatine@example.org", new Address(1L, "00001", "Imperial Palace", "Coruscant", "Galactic Empire"), Gender.MALE),
+            new User(2L, "Obi-Wan", "Kenobi", "obi-wan.kenobi@example.org", new Address(2L, "88420", "Stewjon", "Outer Rim", "Galactic Republic"), Gender.MALE),
+            new User(3L, "Luke", "Skywalker", "luke.skywalker@example.org", new Address(3L, "30777", "Tatooine", "Anchorhead", "Outer Rim Territories"), Gender.MALE),
+            new User(4L, "Lea", "Skywalker", "lea.skywalker@example.org", new Address(4L, "11001", "Aldera", "Alderaan", "Core Worlds"), Gender.FEMALE),
+            new User(5L, "Han", "Solo", "han.solo@example.org", new Address(5L, "50123", "Corellia City", "Coronet", "Independent Systems"), Gender.MALE)
+        )
+    );
+
     public static List<User> getUsers() {
-        return List.of(
-                new User(1L, "Sheev", "Palpatine", "sheev.palpatine@example.org", new Address(1L, "00001", "Imperial Palace", "Coruscant", "Galactic Empire"), Gender.MALE),
-                new User(2L, "Obi-Wan", "Kenobi", "obi-wan.kenobi@example.org", new Address(2L, "88420", "Stewjon", "Outer Rim", "Galactic Republic"), Gender.MALE),
-                new User(3L, "Luke", "Skywalker", "luke.skywalker@example.org", new Address(3L, "30777", "Tatooine", "Anchorhead", "Outer Rim Territories"), Gender.MALE),
-                new User(4L, "Lea", "Skywalker", "lea.skywalker@example.org", new Address(4L, "11001", "Aldera", "Alderaan", "Core Worlds"), Gender.FEMALE),
-                new User(5L, "Han", "Solo", "han.solo@example.org", new Address(5L, "50123", "Corellia City", "Coronet", "Independent Systems"), Gender.MALE)
-        );
+        return UserService.users;
+    }
+
+    public static void addUser(User user) {
+        UserService.users.add(user);
     }
 
     public static List<Address> getAddresses() {
         return List.of(
-                new Address(1L, "00001", "Imperial Palace", "Coruscant", "Galactic Empire"),
-                new Address(2L, "88420", "Stewjon", "Outer Rim", "Galactic Republic"),
-                new Address(3L, "30777", "Tatooine", "Anchorhead", "Outer Rim Territories"),
-                new Address(4L, "11001", "Aldera", "Alderaan", "Core Worlds"),
-                new Address(5L, "50123", "Corellia City", "Coronet", "Independent Systems")
+            new Address(1L, "00001", "Imperial Palace", "Coruscant", "Galactic Empire"),
+            new Address(2L, "88420", "Stewjon", "Outer Rim", "Galactic Republic"),
+            new Address(3L, "30777", "Tatooine", "Anchorhead", "Outer Rim Territories"),
+            new Address(4L, "11001", "Aldera", "Alderaan", "Core Worlds"),
+            new Address(5L, "50123", "Corellia City", "Coronet", "Independent Systems")
         );
     }
 
